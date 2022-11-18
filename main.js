5691d4075e924afeae084e85c520dd67773bdf8f
Avihay Swissa <avihay126@gmail.com>
2022-11-18 15:24:41 +0200

    6

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
-const word=randomVocabulary();
-let levelNumber=1;
+const word = randomVocabulary();
+let levelNumber = 1;
 
 
 // function keyboardListener(){
@@ -20,29 +19,29 @@ let levelNumber=1;
 //         }
 //     })
 // }
-function buttonListener(){
-    const keyboard=document.getElementsByClassName("keyboard_letter");
+function buttonListener() {
+    const keyboard = document.getElementsByClassName("keyboard_letter");
     for (let i = 0; i < keyboard.length; i++) {
-        keyboard[i].addEventListener('click',(event)=>{
-            if (event.target.innerHTML==="Enter"){
-                        checkGuess();
-                    }else if (event.target.innerHTML==="Delete"){
-                        deleteLetter();
-                    }else {
-                         insertChar(event.target.innerHTML);
+        keyboard[i].addEventListener('click', (event) => {
+            if (event.target.innerHTML === "Enter") {
+                checkGuess();
+            } else if (event.target.innerHTML === "Delete") {
+                deleteLetter();
+            } else {
+                insertChar(event.target.innerHTML);
 
-                    }
+            }
         })
     }
 }
 
 
-function insertChar(char){
-    const cells=level();
+function insertChar(char) {
+    const cells = level();
     for (let i = 0; i < cells.length; i++) {
-        if (cells[i].getAttribute('data-state')==='empty'){
-            cells[i].innerHTML=char;
-            cells[i].setAttribute('data-state',char);
+        if (cells[i].getAttribute('data-state') === 'empty') {
+            cells[i].innerHTML = char;
+            cells[i].setAttribute('data-state', char);
             break;
         }
     }
@@ -51,90 +50,80 @@ function insertChar(char){
 function randomVocabulary() {
     document.getElementById('vocabulary');
     const words = ["מקלדת", "מקלחת", "שולחן", "כביסה", "מנעול", "שריפה", "מדפסת",
-        "רמקול", "חולצה", "מדבקה","קרפדה","אכזבה","מעטפה",
-        "מברשת","משאית","מזרון","מגירה","שמיכה", "חשיבה", "מנורה"]
+        "רמקול", "חולצה", "מדבקה", "קרפדה", "אכזבה", "מעטפה",
+        "מברשת", "משאית", "מזרון", "מגירה", "שמיכה", "חשיבה", "מנורה"]
     let randomIndex = Math.ceil(Math.random() * 19);
-    let word=words[randomIndex];
+    let word = words[randomIndex];
     return word;
 
 }
 
-function level(){
-    const level=document.querySelector("div[data-state=\"playing\"]");
-    const cells=level.children;
+function level() {
+    const level = document.querySelector("div[data-state=\"playing\"]");
+    const cells = level.children;
     return cells;
 }
-function gameLoop(){
-alert(word);
-buttonListener();
+
+function gameLoop() {
+    alert(word);
+    buttonListener();
 //keyboardListener();
 
 }
-function checkLength(){
-    const level= document.querySelector("div[data-state=\"playing\"]").children[4];
-    if (level.getAttribute('data-state')==='empty'){
+
+function checkLength() {
+    const level = document.querySelector("div[data-state=\"playing\"]").children[4];
+    if (level.getAttribute('data-state') === 'empty') {
         return false;
     }
     return true;
 }
 
-function checkGuess(){
-    if (checkLength()){
-        const wordLetters=word.split("");
-        const guessLetters=document.querySelector("div[data-state=\"playing\"]").children;
-        let j =0;
-        let flag=true;
-        for (let i = 0; i < guessLetters.length; i++) {
-            if (checkIfLetterExistInWord (guessLetters[j].innerHTML , wordLetters)){
-                if (wordLetters[i]===guessLetters[j].innerHTML){
-                    guessLetters[j].setAttribute('data-color','green');
-                    j++;
-                }else {
-                    guessLetters[j].setAttribute('data-color','yellow');
-                    flag=false;
-                    j++;
-                }
-            } else {
-                guessLetters[j].setAttribute('data-color','gray');
-                flag=false;
+function checkGuess() {
+    if (checkLength()) {
+        const wordLetters = word.split("");
+        const guessLetters = document.querySelector("div[data-state=\"playing\"]").children;
+        let j = 0;
+        let flag = true;
+        for (let i = 0; i < wordLetters.length; i++) {
+            if (guessLetters[i].innerHTML === wordLetters[i]) {
+                guessLetters[i].setAttribute('data-color', 'green');
                 j++;
+            } else if (checkIfLetterExistInWord(guessLetters[i].innerHTML, wordLetters, j)) {
+                guessLetters[i].setAttribute('data-color', 'yellow');
+                j++;
+                flag = false;
+            } else {
+                guessLetters[i].setAttribute('data-color', 'gray');
+                flag = false;
             }
-
         }
-        if (flag===false){
-            document.querySelector("div[data-state=\"playing\"]").setAttribute('data-state',"finish");
+        if (flag === false) {
+            document.querySelector("div[data-state=\"playing\"]").setAttribute('data-state', "finish");
             levelNumber++;
-            if (levelNumber>6){
+            if (levelNumber > 6) {
                 alert("חלש אחי!");
                 document.location.reload();
-            }else {
-                document.getElementById("row"+levelNumber).setAttribute('data-state',"playing");
+            } else {
+                document.getElementById("row" + levelNumber).setAttribute('data-state', "playing");
             }
 
-        }else {
+        } else {
             alert("ניצחת!");
             document.location.reload();
         }
 
 
-    }else {
+    } else {
         alert("guess is shorter than five letters")
     }
 }
-function paintKeyboard(char){
-    const keyboard=document.getElementsByClassName("keyboard_letter");
-    for (let i = 0; i <keyboard.length ; i++) {
-        if (keyboard[i].innerHTML===char);
-        keyboard[i].setAttribute('data-color','green');
-    }
-}
 
-
-function checkIfLetterExistInWord(letter,wordsLetter){
+function checkIfLetterExistInWord(letter, wordsLetter, indexToStart) {
     let flag = false;
-    for (let i =0;  i<wordsLetter.length; i++){
-        if (wordsLetter[i]===letter){
-            flag=true;
+    for (let i = indexToStart; i < wordsLetter.length; i++) {
+        if (wordsLetter[i] === letter) {
+            flag = true;
         }
     }
     return flag;
@@ -142,14 +131,12 @@ function checkIfLetterExistInWord(letter,wordsLetter){
 }
 
 
-
-
-function deleteLetter(){
-    let y=document.querySelector("div[data-state=\"playing\"]").children;
+function deleteLetter() {
+    let y = document.querySelector("div[data-state=\"playing\"]").children;
     for (let i = 0; i < y.length; i++) {
-        if (i===4||y[i].getAttribute('data-state')!=='empty'&&y[i+1].getAttribute('data-state')==='empty'){
-        y[i].setAttribute('data-state','empty')
-        y[i].innerHTML="";
+        if (i === 4 || y[i].getAttribute('data-state') !== 'empty' && y[i + 1].getAttribute('data-state') === 'empty') {
+            y[i].setAttribute('data-state', 'empty')
+            y[i].innerHTML = "";
             return;
         }
     }
